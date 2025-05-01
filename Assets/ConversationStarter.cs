3e1da08f5748dfce88206d8312edf3c35106b77c
mimbies <michelle.koops@haw-hamburg.de>
2025-05-01 20:50:39 +0200

    Dialogue Manager

---
+++ Assets/ConversationStarter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class ConversationStarter : MonoBehaviour
+{
+
+    [SerializeField] private NPCConversation myConvo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConversationManager.Instance.StartConversation(myConvo);
+
+    }
+
+
+}

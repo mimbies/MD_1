ddfe62c1b20feba0e123b28fbb3d872ae8f9a380
mimbies <michelle.koops@haw-hamburg.de>
2025-05-15 12:17:06 +0200

    more lights

--- Assets/Scripts/TriggerSomething.cs
+++ Assets/Scripts/TriggerSomething.cs
@@ -8,12 +8,28 @@ public class TriggerSomething : MonoBehaviour
 
     [SerializeField] private NPCConversation myConvo;
 
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to
+    /// this object (2D physics only).
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            Debug.Log("Trigger");
+            Debug.Log("Entered Lamp Brightness Trigger Zone");
             ConversationManager.Instance.StartConversation(myConvo);
+        }
+
+    }
+
+    /// <summary>
+    /// Sent when another object leaves a trigger collider attached to
+    /// this object (2D physics only).
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player" && this.gameObject.tag != "Tip")
+        {
+            Debug.Log("Left Lamp Brightness Trigger Zone");
+            Destroy(this.gameObject);
         }
 
     }

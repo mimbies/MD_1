7722d9c1e49d636fb8123ea24b9d3ee509dcd45d
mimbies <michelle.koops@haw-hamburg.de>
2025-07-02 19:41:27 +0200

    magnolia collectibles + objectives

---
+++ Assets/CollectibleCount.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleCount : MonoBehaviour
+{
+    TMPro.TMP_Text text;
+    int count;
+
+    void Awake()
+    {
+        text = GetComponent<TMPro.TMP_Text>();
+    }
+    void Start() => UpdateCount();
+
+    void OnEnable() => Collectible.OnCollected += OnCollectibleCollected;
+    void OnDisable() => Collectible.OnCollected -= OnCollectibleCollected;
+
+    void OnCollectibleCollected()
+    {
+        count++;
+        UpdateCount();
+    }
+
+    void UpdateCount()
+    {
+        text.text = $"{count} / 13";
+
+    }
+}

e0d028a3a1b5dc100d00def07c2f5372237d24d1
Cengofat <cengiz1710@hotmail.de>
2025-07-07 15:55:09 +0200

    button spotlights

--- Assets/CengizScripts/ButtonManger.cs
+++ Assets/CengizScripts/ButtonManger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ButtonManger : MonoBehaviour
 {
@@ -8,6 +9,29 @@ public class ButtonManger : MonoBehaviour
     public List<int> correctOrder;
     private List<int> currentInput = new List<int>();
     private int startIndex = 0;
+
+    public List<GameObject> lights;
+    public List<Light2D> spotlights;
+
+    private Color lightOriginalColor;
+
+    void Start()
+    {
+       lightOriginalColor = lights[0].GetComponent<SpriteRenderer>().color;
+
+        foreach(GameObject light in lights)
+        {
+            Light2D childLight = light.GetComponentInChildren<Light2D>();
+            spotlights.Add(childLight);
+        }
+        foreach(Light2D light in spotlights)
+        {
+            light.enabled = false;
+        }
+
+
+
+    }
 
     public void ButtonPressed(int id)
     {
@@ -18,13 +42,25 @@ public class ButtonManger : MonoBehaviour
             if (startIndex == -1)
             {
                 Debug.Log("Ungültiger Startknopf!");
-                ResetButtons();
+                ResetButtons(id);
                 return;
             }
 
             currentInput.Add(id);
+            spotlights[correctOrder.IndexOf(id)].GetComponent<Light2D>().enabled = true;
+            spotlights[correctOrder.IndexOf(id)].GetComponent<Light2D>().color = Color.yellow;
+            lights[correctOrder.IndexOf(id)].GetComponent<SpriteRenderer>().color = Color.yellow;
+            
+
             return;
         }
+
+        lights[correctOrder.IndexOf(id)].GetComponent<SpriteRenderer>().color = Color.yellow;
+
+        spotlights[correctOrder.IndexOf(id)].GetComponent<Light2D>().enabled = true;
+        spotlights[correctOrder.IndexOf(id)].GetComponent<Light2D>().color = Color.yellow;
+
+
 
         int expectedIndex = (startIndex + currentInput.Count) % correctOrder.Count;
         int expectedID = correctOrder[expectedIndex];
@@ -33,7 +69,7 @@ public class ButtonManger : MonoBehaviour
         if (id != expectedID)
         {
             Debug.Log("Falsche Reihenfolge! Reset.");
-            ResetButtons();
+            ResetButtons(id);
             return;
         }
 
@@ -60,14 +96,66 @@ public class ButtonManger : MonoBehaviour
                 button.SetColor(Color.green);
             }
 
+            StartCoroutine(FlashGreen());
+
+
         }
 
 
     }
-    public void ResetButtons()
+    private IEnumerator FlashGreen()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            foreach (Light2D light in spotlights)
+            {
+                light.enabled = false;
+            }
+            yield return new WaitForSeconds(0.3f);
+
+            foreach (Light2D light in spotlights)
+            {
+                light.enabled = true;
+                light.color = Color.green;
+            }
+            yield return new WaitForSeconds(0.3f);
+
+        }
+
+
+
+    }
+
+    public void ResetButtons(int id)
     {
         currentInput.Clear();
         startIndex = 0;
+        StartCoroutine(FlashRedThenReset(id));
+
+    }
+
+    private IEnumerator FlashRedThenReset(int id)
+    {
+        lights[correctOrder.IndexOf(id)].GetComponent<SpriteRenderer>().color = Color.red;
+
+        spotlights[correctOrder.IndexOf(id)].GetComponent<Light2D>().enabled = true;
+        spotlights[correctOrder.IndexOf(id)].GetComponent<Light2D>().color = Color.red;
+        yield return new WaitForSeconds(1f);
+
+
+        foreach (Light2D light in spotlights)
+        {
+            light.enabled = false;
+        }
+
+
+        foreach (GameObject light in lights)
+        {
+            light.GetComponent<SpriteRenderer>().color = lightOriginalColor;
+            
+            
+        }
+
         foreach (ButtonPuzzle button in buttons)
         {
             button.ResetButton();
@@ -75,10 +163,7 @@ public class ButtonManger : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()

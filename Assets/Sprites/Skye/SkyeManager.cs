58e0b37748920e0a35ec2cf3d11e0563560a97aa
akaiiri <115646743+akaiiri@users.noreply.github.com>
2025-07-08 13:38:29 +0200

    add sprites and replace cat

--- Assets/Sprites/Skye/SkyeManager.cs
+++ Assets/Sprites/Skye/SkyeManager.cs
@@ -30,6 +30,17 @@ public class SkyeManager : MonoBehaviour
         }
     }
 
+    private void LateUpdate()
+    {
+        if (transform.position.y < target.position.y)
+        {
+            sprite.sortingOrder = 1;
+        } else
+        {
+            sprite.sortingOrder = -1;
+        }
+    }
+
     public void EnableFollow()
     {
         followActive = true;
